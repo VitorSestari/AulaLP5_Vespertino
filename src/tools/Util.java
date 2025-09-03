bf2ccf76c72418cf137daee46ff9ce3859b7d23e
VitorSestari <vitor.carvalho@estudante.ifms.edu.br>
2025-09-03 17:19:36 -0400

    Terminamos o limpar parcialmente e criamos metodos de conversão de tipos

--- src/tools/Util.java
+++ src/tools/Util.java
@@ -4,8 +4,15 @@
  */
 package tools;
 
+import java.util.Date;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+
+
 
 /**
  *
@@ -21,10 +28,44 @@ public class Util {
     
     public static void limpar(JComponent ... componentes){
         for (int i = 0; i < componentes.length; i++) {
+            if(componentes [i] instanceof JTextField){
             ((JTextField)componentes[i]).setText("");
-            
+            }
+            if(componentes [i] instanceof JComboBox){
+                ((JComboBox)componentes[i]).setSelectedIndex(-1);
+            }
+            if(componentes [i] instanceof JFormattedTextField){
+                ((JFormattedTextField)componentes[i]).setText("");
+            }
         }
         
     }
+    
   
+
+public static void mensagem(String cad){
+    JOptionPane.showMessageDialog(null, cad);
+}
+public static boolean pergunta(String cad){
+    JOptionPane.showConfirmDialog(null, cad);
+    return true;
+}
+public static int strToInt(String num){
+    return Integer.parseInt(num); 
+}
+public static String IntTostr(int num){
+    return String.valueOf(num);
+}
+public static double strToDouble(double num){
+    return 0.0;
+}
+public static String DoubleTostr(String num){
+    return " ";
+}
+public static Date strToData(Date num){
+    return null;
+}
+public static String DataTostr(String num){
+    return " ";
+}
 }
